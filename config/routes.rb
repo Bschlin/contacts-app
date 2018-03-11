8601a03f5669cef5b4146d267e7b7105dff05578
Blake Schlin <blakeschlin@gmail.com>
2018-03-11 11:34:43 -0500

    implemented all 5 RESTful HTTP actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   namespace :v1 do
-     get "/contact_list_url" => "contacts#first_contact_method"
-     get "/contact_list1_url" => "contacts#second_contact_method"
-     get "/contact_list2_url" => "contacts#third_contact_method"
-   end 
+  namespace :v1 do
+     # get "/contact_list_url" => "contacts#first_contact_method"
+     # get "/contact_list1_url" => "contacts#second_contact_method"
+     # get "/contact_list2_url" => "contacts#third_contact_method"
+    get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
+  end 
 end
--- contact_list.rb
+++ contact_list.rb
@@ -2,22 +2,73 @@ require "unirest"
 
 system "clear"
 puts "Choose an option"
-puts "[1] Contact 1"
-puts "[2] Contact 2"
-puts "[3] Contact 3"
+puts "[1] See Contact list"
+puts "[2] See one Contact"
+puts "[3] Create a Contact"
+puts "[4] Update a Contact"
+puts "[5] Delete a Contact"
+
+# input_option = gets.chomp
+# if input_option == "1"
+#   response = Unirest.get("http://localhost:3000/v1/contact_list_url")
+#   contact = response.body
+#   puts JSON.pretty_generate(contact)
+# elsif input_option == "2"
+#   response = Unirest.get("http://localhost:3000/v1/contact_list1_url")
+#   contact = response.body
+#    puts JSON.pretty_generate(contact)
+# elsif input_option == "3"
+#   response = Unirest.get("http://localhost:3000/v1/contact_list2_url")
+#   contact = response.body
+#   puts JSON.pretty_generate(contact)
+# end
 
 input_option = gets.chomp
 if input_option == "1"
-  response = Unirest.get("http://localhost:3000/v1/contact_list_url")
+  response = Unirest.get("http://localhost:3000/v1/contacts")
   contact = response.body
   puts JSON.pretty_generate(contact)
 elsif input_option == "2"
-  response = Unirest.get("http://localhost:3000/v1/contact_list1_url")
-  contact = response.body
-   puts JSON.pretty_generate(contact)
-elsif input_option == "3"
-  response = Unirest.get("http://localhost:3000/v1/contact_list2_url")
+  print "Enter a contact id: "
+  contact_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contacts/#{contact_id}")
   contact = response.body
   puts JSON.pretty_generate(contact)
-end
-
+elsif input_option == "3"
+ params = {}
+ print "first name: "
+ params["input_first_name"] = gets.chomp
+ print "last name: "
+ params["input_last_name"] = gets.chomp
+ print "email: "
+ params["input_email"] = gets.chomp
+ print "phone number: "
+ params["input_phone_number"] = gets.chomp
+ response = Unirest.post("http://localhost:3000/v1/contacts", parameters: params)
+ contact = response.body 
+ puts JSON.pretty_generate(contact)
+elsif input_option == "4"
+  print "Enter a contact id: "
+  contact_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contact/#{contact_id}")
+  contact = response.body
+  params = {}
+  print "first name(#{contact["first name"]}): "
+  params["input_first_name"] = gets.chomp
+  print "last name (#{contact["last name"]}): "
+  params["input_last_name"] = gets.chomp
+  print "email (#{contact["email"]}): "
+  params["input_email"] = gets.chomp
+  print "phone number (#{contact["phone number"]}): "
+  params["input_phone_number"] = gets.chomp
+  params.delete_if { |_key, value| value.empty? }
+  response = Unirest.patch("http://localhost:3000/v1/contacts/#{contact_id}", parameters: params)
+  contact = response.body
+  puts JSON.pretty_generate(contact)
+elsif input_option == "5"
+  print "Enter a contact id: "
+  contact_id = gets.chomp
+  response = Unirest.delete("http://localhost:3000/v1/contacts/#{contact_id}")
+  body = response.body 
+  puts JSON.pretty_generate(body)
+end 
