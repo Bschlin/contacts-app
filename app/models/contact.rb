07bfeb047bb3a6d97fce55a5416394492abb0e29
Blake Schlin <blakeschlin@gmail.com>
2018-03-12 20:08:23 -0500

    use model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,28 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+  end
+
+  def prefix
+    "+81 #{phone_number}"
+  end 
+
+
+  def as_json
+    {
+      id: id,
+      full_name: full_name,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      phone_number: prefix,
+      created_at: friendly_updated_at
+    }
+  end
+
 end
